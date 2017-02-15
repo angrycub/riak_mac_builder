87e4725e9d70a952acc2b76546beb5cd59dba924
Charlie Voiselle <cvoiselle@basho.com>
2017-02-15 11:14:41 -0500

    Still working on test provisioner

--- provision-test.sh
+++ provision-test.sh
@@ -11,3 +11,31 @@ echo "
 kern.maxfiles=524288
 kern.maxfilesperproc=262144
 " >> /etc/sysctl.conf
+
+echo "
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+<key>Label</key>
+<string>limit.maxfiles</string>
+<key>ProgramArguments</key>
+<array>
+<string>launchctl</string>
+<string>limit</string>
+<string>maxfiles</string>
+<string>262144</string>
+<string>262144</string>
+</array>
+<key>RunAtLoad</key>
+<true/>
+<key>ServiceIPC</key>
+<false/>
+</dict>
+</plist>
+" >> /Library/LaunchDaemons/limit.maxfiles.plist
+
+echo "* Upping ulimit in .profile"
+echo "
+ulimit -n 65536
+" >> /Users/vagrant/.profile

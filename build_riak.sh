dc9d8dbb42028c5811a168c9c637ce9a8c33512b
Charlie Voiselle <cvoiselle@basho.com>
2017-03-23 21:24:43 -0400

    Rev'd to 2.2.2; working on git issues
    Had some problems with git over https for the non-ee repo. Switched it
    globally to git to work around them.  Not loving it, but it did let
    things work

--- build_riak.sh
+++ build_riak.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
-export RIAK_VERSION=2.2.1
+export RIAK_VERSION=2.2.2
 
+# Added this hack to get around some weirdness with fetching over
+# https on my builder instance
+git config --global url."git://".insteadOf https://
 
 print_time () {
 	echo "==========  $(date)  =========="
@@ -20,7 +23,8 @@ cp package/packages/* /vagrant/packages
 print_time
 
 echo "* Cloning Riak Repo"
-git clone https://github.com/basho/riak
+cd ~
+git clone git://github.com/basho/riak
 cd riak
 git checkout riak-${RIAK_VERSION}
 print_time
